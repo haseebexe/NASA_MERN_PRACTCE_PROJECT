cb3704556b112989ae1a06b901203ebf221c96a9
Muhammad Haseeb <muhammadhaseeb@crunchcraft.com>
2026-02-09 17:47:47 +0500

    Initial commit - fullstack React + Node project

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,34 @@
+// Load planets and return as JSON.
+const API_BASE_URL = "http://localhost:8000"
+
+async function httpGetPlanets() {
+   const response = await fetch(`${API_BASE_URL}/planets`);
+   return await response.json();
+}
+
+ // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+ const response = await fetch(`${API_BASE_URL}/launches`)
+ const fetchedLaunches = await response.json();
+ return fetchedLaunches.sort((a, b)=> {
+  return a.flightNumber - b.flightNumber;
+ })
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+const express =  require('express');
+const cors = require('cors');
+const path = require("path");
+const morgan =  require('morgan');
+const planetsRouter = require('./routes/planets/planets.route');
+const launchesRouter = require('./routes/launches/launches.routes')
+
+const app = express();
+
+
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+app.use(morgan('combined'))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "..", "public")))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public" , "index.html"))
+})
+app.use((req, res, next) => {
+  console.log('Request IP is', req.ip);
+  next();
+});
+
+app.use(planetsRouter)
+app.use(launchesRouter)
+
+module.exports = app;
