0bc4793d250458998d9520dfdea95371fa919d5f
Muhammad Haseeb <muhammadhaseeb@crunchcraft.com>
2026-02-26 01:49:28 +0500

    removed object id and version from api response.

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
-const path =  require('path');
+const path = require("path");
 const { parse } = require("csv-parse");
 
-const habitablePlanets = [];
+const planets = require("./planets.mongo");
+
+// const habitablePlanets = [];
 
 const isHabitablePlanet = (planet) => {
   return (
@@ -15,32 +17,54 @@ const isHabitablePlanet = (planet) => {
 
 function getPlanetsData() {
   return new Promise((resolve, reject) => {
-    fs.createReadStream(path.join(__dirname, "..", "..", "data" , "kepler_data.csv"))
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv"),
+    )
       .pipe(
         parse({
           comment: "#",
           columns: true,
         }),
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          await savePlanet(data);
         }
       })
       .on("error", (err) => {
         console.log(err);
-        reject()
+        reject();
       })
-      .on("end", () => {
-        console.log("done");
-        resolve()
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found`);
+        resolve();
       });
   });
 }
 
+async function getAllPlanets() {
+  return await planets.find({}, {
+    '_id': 0, '__v': 0,
+  } );
+}
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      },
+    );
+  } catch (err) {
+    console.error(`Could not save plaent ${err}`);
+  }
 }
 
 module.exports = {
--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,28 @@
 const http = require('http');
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config({ path: '../.env' });
 
 const { getPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@nasacluster.zwawkr6.mongodb.net/${process.env.DB_NAME}?appName=NasaCluster`
+
+mongoose.connection.once('open', ()=> {
+    console.log('Database is ready!')
+})
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
 
 console.log(PORT)
 
 const server = http.createServer(app);
 
 async function startServer() {
+    await mongoose.connect(MONGO_URL)
+
     await getPlanetsData()
 
     server.listen(PORT, ()=> {
